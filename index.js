e5acce95dee2f143702b8508a126b797de1ac318
mawalu <web@mawalabs.de>
2018-10-29 14:56:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = class Tasks {
+  static name () {
+    return 'tasks'
+  }
+
+  constructor ({ cache, logger, config, program }) {
+    this.cache = cache
+    this.logger = logger
+    this.config = config
+    this.program = program
+
+    program 
+      .command('task:run')
+      .arguments('<name>')      
+      .action(this.run.bind(this))
+  }
+
+  run (name) {
+    return this.config.tasks[name].map(task => {
+      return this.program.parse(['', '', ...task.split(' ')])
+    })
+  }
+}
